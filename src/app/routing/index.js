d273fb37f0ba77efe33f0dad96797705c76cdba2
hadi <tayebi1426@mail.com>
2019-07-24 17:48:18 +0430

    tab

--- src/app/routing/index.js
+++ src/app/routing/index.js
@@ -1,9 +1,11 @@
 import FormsUi from "../ui/FormsUi";
 import GridUi from "../ui/GridUi";
+import TabComponent from "../ui/TabComponent";
 
 const MAIN_ROUTES = [
     {path: '/form', component: FormsUi},
-    {path: '/grid', component: GridUi}
+    {path: '/grid', component: GridUi},
+    {path: '/tab', component: TabComponent}
 ];
 
 export default MAIN_ROUTES;
