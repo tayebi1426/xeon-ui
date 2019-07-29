c09f034a0aac76b88702bee674f4412bed1bc751
r.aghajani <r.aghajani.demis@gmail.com>
2019-07-29 08:29:04 +0430

    propTypes added to DataGrid

--- src/core/components/grid/DataGrid.js
+++ src/core/components/grid/DataGrid.js
@@ -4,6 +4,8 @@ import {withTranslation} from '../../lib'
 import GridColumn from './GridColumn'
 import GridCommands from './GridCommands'
 import GridCommand from './GridCommand'
+import * as PropTypes from 'prop-types';
+import Button from "../form/Button";
 
 class DataGrid extends React.Component {
 
@@ -47,19 +49,87 @@ class DataGrid extends React.Component {
                 cell: this.createCustomCommandCell.bind(this, gridCommands)
             });
     }
-    createCustomCommandCell (gridCommands, props) {
+
+    createCustomCommandCell(gridCommands, props) {
         let {children} = gridCommands.props;
-        let actions = React.Children.toArray(children).map((child,idx) => {
-            return React.createElement(GridCommand,{
-                key:idx,
+        let actions = React.Children.toArray(children).map((child, idx) => {
+            return React.createElement(GridCommand, {
+                key: idx,
                 ...child.props,
-                ...props});
+                ...props
+            });
         });
         return <td>
             {actions}
         </td>
     }
 }
+
+DataGrid.propTypes = {
+    data: PropTypes.oneOfType([PropTypes.array, PropTypes.shape({
+        data: PropTypes.array,
+        total: PropTypes.number
+    })]),
+    sortable: PropTypes.oneOfType([PropTypes.bool, PropTypes.shape({
+        mode: PropTypes.string,
+        allowUnsort: PropTypes.bool
+    })]),
+    sort: PropTypes.array,
+    onSortChange: PropTypes.func,
+    filterable: PropTypes.bool,
+    filter: PropTypes.object,
+    onFilterChange: PropTypes.func,
+    pageable: PropTypes.oneOfType([PropTypes.bool, PropTypes.shape({
+        buttonCount: PropTypes.number,
+        info: PropTypes.bool,
+        type: PropTypes.string,
+        pageSizes: PropTypes.oneOfType([PropTypes.bool, PropTypes.arrayOf(PropTypes.number)]),
+        previousNext: PropTypes.bool,
+    })]),
+    pageSize: PropTypes.number,
+    onPageChange: PropTypes.func,
+    total: PropTypes.number,
+    skip: PropTypes.number,
+    take: PropTypes.number,
+    onExpandChange: PropTypes.func,
+    expandField: PropTypes.string,
+    selectedField: PropTypes.string,
+    onSelectionChange: PropTypes.func,
+    onHeaderSelectionChange: PropTypes.func,
+    resizable: PropTypes.bool,
+    reorderable: PropTypes.bool,
+    group: PropTypes.any,
+    groupable: PropTypes.oneOfType([PropTypes.bool, PropTypes.shape({
+        enabled: PropTypes.bool,
+        footer: PropTypes.string
+    })]),
+    onGroupChange: PropTypes.func,
+    onRowClick: PropTypes.func,
+    onItemChange: PropTypes.func,
+    editField: PropTypes.string,
+    scrollable: PropTypes.string,
+    rowHeight: PropTypes.number,
+    detail: PropTypes.any,
+    style: PropTypes.object,
+    onDataStateChange: PropTypes.func,
+    onColumnResize: PropTypes.func,
+    onColumnReorder: PropTypes.func
+};
+
+Button.defaultProps = {
+    data: [],
+    pageSize: 10,
+    sortable: true,
+    sort: [],
+    filterable: true,
+    filter: {},
+    pageable: true,
+    skip: 0,
+    take: 10,
+    resizable: false,
+    reorderable: false,
+    groupable: false,
+};
 
 export default withTranslation(DataGrid);
 
