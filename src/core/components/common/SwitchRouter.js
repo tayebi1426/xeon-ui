c847646c25f3b2cc8d4e4814e48f02a8a7b72ad7
hadi <tayebi1426@mail.com>
2019-08-06 15:25:46 +0430

    SwitchRouter children routs

--- src/core/components/common/SwitchRouter.js
+++ src/core/components/common/SwitchRouter.js
@@ -1,12 +1,17 @@
 import React from "react";
 import {Route, Switch} from 'react-router-dom';
+import PropTypes from "prop-types";
 
-function SwitchRouter ({routes}){
-        return <Switch>
-            {
-                routes && routes.map(r => <Route key={r.path} {...r}/>)
-            }
-        </Switch>
+function SwitchRouter({routes, children}) {
+    return <Switch>
+        {
+            (routes && routes.length > 0) ? routes.map(r => <Route key={r.path} {...r}/>) : children
+        }
+    </Switch>
 }
+
+SwitchRouter.propTypes = {
+    routes: PropTypes.array
+};
 
 export default SwitchRouter;
