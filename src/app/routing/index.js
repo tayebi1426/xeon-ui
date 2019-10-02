85be365b79bf3a7d434ee1b071024ea68b0d7f82
a.sadeghi <a.sadeghi@demisco.com>
2019-10-02 16:10:14 +0330

    Add card,Add icon to button

--- src/app/routing/index.js
+++ src/app/routing/index.js
@@ -4,12 +4,16 @@ import Login from "../ui/Login";
 
 let FormsUi = React.lazy(() => import("../ui/FormsUi"));
 let GridUi = React.lazy(() => import("../ui/GridUi"));
+let FraudGrid = React.lazy(() => import("../ui/FraudGrid"));
 let TabComponent = React.lazy(() => import("../ui/TabComponent"));
 let SearchGithub = React.lazy(() => import("../ui/SearchGithub"));
+let Test = React.lazy(() => import("../ui/Test"));
 
 const MAIN_ROUTES = [
     {path: '/form', component: FormsUi,roles:['ADMIN']},
     {path: '/grid', component: GridUi},
+    {path: '/gridTest', component: FraudGrid},
+    {path: '/test', component: Test},
     {path: '/tab', component: TabComponent,access:[]},
     {path: '/searchGithub', component: SearchGithub}
 ];
--- src/core/components/CustomBootstrap/Card.js
+++ src/core/components/CustomBootstrap/Card.js
@@ -1,31 +1,113 @@
-import React from "react";
+import React, {useState} from "react";
+import {Animated} from "react-animated-css";
 import PropTypes from "prop-types";
-import {Card as BsCard, CardBody, CardTitle} from 'reactstrap'
+import {Card as BsCard, CardBody, CardTitle,Collapse} from 'reactstrap'
 import {I18Massage} from "../common";
+import Button from "../form/Button";
 
-const Card = ({title = '', icon = null, children}) => {
+
+const Card = ({title = '', icon = null, children, isCollapse, collapseConfig}) => {
+
+    const [collapse,setState] = useState(false);
+
+    function toggle() {
+        collapse ? setState(false) : setState(true);
+    }
+
+    function setTitleAndIcon() {
+        let config = {
+            closeTitle:"بسته",
+            openTitle:"باز",
+            closeIcon:"fa fa-angle-up fa-lg",
+            openIcon:"fa fa-angle-down fa-lg"
+        };
+        if (collapseConfig) {
+            config.closeTitle = collapseConfig.closeTitle;
+            config.openTitle = collapseConfig.openTitle;
+        }
+        return config;
+    }
+
+    function setAnimation() {
+        let anim = {
+            animationIn:"" ,
+            animationOut:""
+        };
+        if(collapseConfig) {
+            anim.animationIn = collapseConfig.animationIn;
+            anim.animationOut = collapseConfig.animationOut;
+        }
+        return anim;
+    }
+
     function renderIcon() {
-        if(icon !== '') {
+        if (icon !== '') {
             return (
-                <div className="col pull-left" dangerouslySetInnerHTML={{ __html: icon }}/>
+                <div className="col pull-left" dangerouslySetInnerHTML={{__html: icon}}/>
             )
         }
     }
-    return (<BsCard>
-            <CardBody>
-                <CardTitle className="text-bold">
+
+    function renderBody() {
+        const {animationIn,animationOut} = setAnimation();
+        return (
+            collapse ?
+                <Animated animationIn={animationIn} animationOut={animationOut} isVisible={true}>
+                <CardBody>
+                        {children}
+                    </CardBody>
+                </Animated>
+
+                :
+                null
+        )
+    }
+
+    function renderCollapse() {
+        const {closeTitle,openTitle,closeIcon,openIcon} = setTitleAndIcon(collapseConfig);
+        if (isCollapse) {
+            return (
+                collapse ?
+                    <Button color="white" title={closeTitle}
+                            onClick={toggle}
+                            icon={closeIcon}
+                            className="cursor-pointer pull-left px-2"
+                    />
+                    :
+                    <Button color="white" title={openTitle}
+                            onClick={toggle}
+                            icon={openIcon}
+                            className="cursor-pointer pull-left px-2"
+                    />
+            )
+        }
+    }
+
+    return (
+        <div>
+            <BsCard>
+                <CardTitle className="text-bold px-3 py-2">
                     <div className="row">
                         <div className="col"><I18Massage code={title}/></div>
                         {renderIcon()}
+                        {renderCollapse()}
                     </div>
                 </CardTitle>
-                {children}
-            </CardBody>
-        </BsCard>
+                <Collapse
+                isOpen={collapse}
+            >
+
+                    {renderBody()}
+
+            </Collapse>
+            </BsCard>
+        </div>
     );
 };
 
 Card.propTypes = {
+    collapseConfig: PropTypes.object,
+    isCollapse: PropTypes.bool,
     title: PropTypes.string,
     icon: PropTypes.string
 };
