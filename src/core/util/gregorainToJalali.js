ed9f59138ac74cc54431f7777cac023361c45074
a.sadeghi <a.sadeghi@demisco.com>
2019-10-08 15:09:02 +0330

    fix bug in gregorianToJalali function verson 2

--- src/core/util/gregorainToJalali.js
+++ src/core/util/gregorainToJalali.js
@@ -1,19 +1,16 @@
-const gregorianToJalali = d => {
-    const isDate = isValidDate(d);
-    if(isDate) {
-        let date = new Date(d);
-        return date.toLocaleDateString("fa-IR", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-        });
-    }else {
-        return d;
+const gregorianToJalali = dt => {
+    if(!dt){
+        return;
     }
+    let date = new Date(dt);
+        if(date != 'Invalid Date') {
+            return date.toLocaleDateString("fa-IR", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+            });
+        }
+        return dt
 };
-
-function isValidDate(d) {
-    return d instanceof Date && !isNaN(d);
-}
 
 export default gregorianToJalali;
