091e1ddcdf9321b07fb7b2dac4ffcbca36ed5fcf
hadi <tayebi1426@mail.com>
2019-11-10 09:03:29 +0330

    Col

--- src/core/components/bootstrap/Col.js
+++ src/core/components/bootstrap/Col.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Col as _Col} from "reactstrap";
+import {Col as BsCol} from "reactstrap";
 import PropTypes from 'prop-types'
 const Col = (props) => (
-    <_Col {...props} widths={['xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl']}/>
+    <BsCol {...props} widths={['xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl']}/>
 );
 Col.propTypes ={
     className:PropTypes.string
