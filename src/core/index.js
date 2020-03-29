9ed0bff66b840a4fbf303c605db284253fba7c2e
hadi <tayebi1426@gmail.com>
2020-03-29 15:22:04 +0430

    fix start errors

--- src/core/index.js
+++ src/core/index.js
@@ -1,14 +1,14 @@
 import XeonApp from './base/XeonApp'
 
-import ('./assets/css/vendor/bootstrap-rtl.min.css');
-import ('./assets/css/vendor/animate.min.css');
-import ('./assets/webfonts/fontawesome.css');
-import ('./assets/css/notification/notifications.css');
-import ('./assets/css/vendor/kendo-all.css');
-import ('./assets/css/sass/themes/gogo.light.green.scss');
-import ('./assets/fonts/iransans/style.css');
-//import ('./assets/css/sass/custom-kendo.scss');
-import ('./assets/css/sass/style.scss');
+import './assets/css/vendor/bootstrap-rtl.min.css';
+import './assets/css/vendor/animate.min.css';
+import './assets/webfonts/fontawesome.css';
+import './assets/css/notification/notifications.css';
+import './assets/css/vendor/kendo-all.css';
+import './assets/css/sass/themes/gogo.light.green.scss';
+import './assets/fonts/iransans/style.css';
+//import './assets/css/sass/custom-kendo.scss';
+import './assets/css/sass/style.scss';
 
 export * from './components/grid/index';
 export * from './components/tab/index';
